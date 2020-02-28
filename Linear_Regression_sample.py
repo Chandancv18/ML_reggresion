1c1a0140662b5efa91408255660c8e291d7cdb01
Chandancv18 <58333517+Chandancv18@users.noreply.github.com>
2020-02-28 22:47:04 +0530

    Create Linear_Regression_sample.py

---
+++ Linear_Regression_sample.py
@@ -0,0 +1,25 @@
+from sklearn.linear_model import LinearRegression
+regressor_linear = LinearRegression()
+regressor_linear.fit(X_train, y_train)
+
+
+
+from sklearn.metrics import r2_score
+
+# Predicting Cross Validation Score the Test set results
+cv_linear = cross_val_score(estimator = regressor_linear, X = X_train, y = y_train, cv = 10)
+
+# Predicting R2 Score the Train set results
+y_pred_linear_train = regressor_linear.predict(X_train)
+r2_score_linear_train = r2_score(y_train, y_pred_linear_train)
+
+# Predicting R2 Score the Test set results
+y_pred_linear_test = regressor_linear.predict(X_test)
+r2_score_linear_test = r2_score(y_test, y_pred_linear_test)
+
+# Predicting RMSE the Test set results
+rmse_linear = (np.sqrt(mean_squared_error(y_test, y_pred_linear_test)))
+print("CV: ", cv_linear.mean())
+print('R2_score (train): ', r2_score_linear_train)
+print('R2_score (test): ', r2_score_linear_test)
+print("RMSE: ", rmse_linear)
